df39795f32725696a1905435130a665b46e5eb6c
Denzel Irizarry Cotts <denzel_irizarry@outlook.com>
2026-02-19 14:27:11 -0800

    updated  to test different varaibles and issue with logic

--- Program.cs
+++ Program.cs
@@ -8,60 +8,56 @@ namespace Week6ChallengeLab
         {
             //initialize array
             int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-
-            //for (int i = matrix.GetLength(0) - 1; i >= 0; i--)
-            //{
-            //    int count = 0;
-            //    Console.WriteLine(matrix[i, count]);
-            //    //Console.WriteLine(matrix[count,i]);
-            //    count++;
-            //    Console.WriteLine("second for starts here");
-            //    for (int j = 0; j < matrix.GetLength(1); j++)
-            //    {
-
-            //        Console.WriteLine(matrix[j, i]);
-
-            //    }
-            //    Console.WriteLine("third for start here");
-            //    for (int j = matrix.GetLength(1) - 1; j >= 0; j--)
-            //    {
-            //        Console.WriteLine(matrix[j, i]);
-            //    }
-
-            //    Console.WriteLine();
-            //}
-
-            int matrixLength = matrix.GetLength(0);
-
-            for (int i = 0; i < matrixLength; i++)
-            {
-                for (int j = matrixLength - 1; j >= 0; j--)
-                {
-                    int temp = matrix[i, j];
-                    matrix[i,j] = matrix[j, i];
-                    matrix[j, i] = temp;
-                }
-            }
+            int[,] matrix2 = { { 5, 1, 9, 11 }, { 2, 4, 8, 10 }, { 13, 3, 6, 7 }, { 15, 14, 12, 16 } };
+            int[,] reverse = RotateArray(matrix);
+            int[,] reverse2 = RotateArray(matrix2);
             
-            for (int i = 0; i < matrixLength; i++)
+            for(int i = 0; i < reverse.GetLength(0); i++)
             {
-                for(int j= 0; j < matrixLength / 2; j++)
+                for(int j = 0; j < reverse.GetLength(1); j++)
                 {
-                    int temp = matrix[i,j];
-                    matrix[i,j] = matrix[i,matrixLength -1 - j];
-                    matrix[i,matrixLength -1 - j] = temp;
+                    Console.Write(reverse[i,j] + " ");
                 }
+                Console.WriteLine();
             }
-            
-            for(int i = 0; i < matrix.GetLength(0); i++)
+            Console.WriteLine();
+
+
+            for(int i = 0; i < reverse2.GetLength(0); i++)
             {
-                for(int j = 0; j < matrix.GetLength(1); j++)
+                for(int j = 0; j < reverse2.GetLength(1); j++)
                 {
-                    Console.Write(matrix[i,j] + " ");
+                    Console.Write(reverse2[i,j] + " ");
                 }
                 Console.WriteLine();
             }
 
+        }
+        static int[,] RotateArray(int[,] matrix)
+        {
+            int matrixLength = matrix.GetLength(0);
+
+            for (int i = 0; i < matrixLength; i++)
+            {
+                for (int j = i; j < matrixLength; j++)
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[j, i];
+                    matrix[j, i] = temp;
+                }
+            }
+
+            for (int i = 0; i < matrixLength; i++)
+            {
+                for (int j = 0; j < matrixLength / 2; j++)
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, matrixLength - 1 - j];
+                    matrix[i, matrixLength - 1 - j] = temp;
+                }
+            }
+
+            return matrix;
         }
        
     }
